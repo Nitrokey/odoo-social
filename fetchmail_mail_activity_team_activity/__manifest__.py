cdc94fa7d2b1448335e5c1b5834bbfd96bcc20fb
dsolanki <dhara.solanki@initos.com>
2023-01-04 10:11:39 +0530

    [CHANGE] Changed module technical name

--- fetchmail_mail_activity_team_activity/__manifest__.py
+++ fetchmail_mail_activity_team_activity/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Mail Activity on Fetchmail",
+    "name": "Mail Activity on Fetchmail with Team Activity",
     "version": "15.0.1.0.0",
     "development_status": "Alpha",
     "category": "Hidden",
