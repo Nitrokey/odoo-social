e202dac3b0a16f72f714dfc7625fff4fed8eb887
Jan Suhr <jan@nitrokey.com>
2025-07-23 20:05:57 +0200

    remove websocket

--- mail_gateway/models/__init__.py
+++ mail_gateway/models/__init__.py
@@ -2,7 +2,6 @@ from . import mail_message
 from . import mail_notification
 from . import mail_channel
 from . import mail_gateway
-from . import ir_websocket
 from . import res_partner
 from . import mail_guest
 from . import mail_gateway_abstract
