fdc1d4a3ee7dd73a2f7173db7f7d64af53a8a788
Jan Suhr <jan@nitrokey.com>
2025-09-24 16:01:15 +0200

    mail_gateway: fix copyright

--- mail_gateway/controllers/discuss.py
+++ mail_gateway/controllers/discuss.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Dixmit
+# Copyright 2025 Nitrokey GmbH
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo.addons.mail.controllers.discuss import DiscussController
