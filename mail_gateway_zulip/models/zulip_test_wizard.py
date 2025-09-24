be76c05404f75c33684ab384b95bf29694494a41
Jan Suhr <jan@nitrokey.com>
2025-09-24 16:00:54 +0200

    fix copyright

--- mail_gateway_zulip/models/zulip_test_wizard.py
+++ mail_gateway_zulip/models/zulip_test_wizard.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Dixmit
+# Copyright 2025 Nitrokey GmbH
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
--- mail_gateway_zulip/tests/__init__.py
+++ mail_gateway_zulip/tests/__init__.py
@@ -1,2 +1,2 @@
-# Copyright 2024 Dixmit
+# Copyright 2025 Nitrokey GmbH
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
